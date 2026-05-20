16326f5ab238d620caa53d8186100078e0885c75
August Öfverström <august.ofverstrom@hotmail.com>
2026-05-20 11:04:55 +0200

    Add config/Esp32s3.cpp and Esp32s3.h

--- main/include/driver/config/esp32s3.h
+++ main/include/driver/config/esp32s3.h
@@ -25,5 +25,21 @@ public:
         // Lägg i en cpp-fil och inkludera också freeRTOS.h och task.h där, se main.cpp
         vTaskDelay(pdMS_TO_TICKS(ms));
     }
+    const adc::Settings& getADC(adc::Id id) const noexcept override;
+
+    const gpio::Settings& getGpio(gpio::Id id) const noexcept override;
+
+    const timer::Settings& getTimer(timer::Id id) const noexcept override; 
+
+    const serial::Settings& getSerial() const noexcept override;
+
+    const tempsensor::Settings& getTempsensor() const noexcept override;
+
+private:
+    std::array<adc::Esp32s3Settings, static_cast<std::size_t>(adc::Id::COUNT)> adcConfigs_; 
+    std::array<gpio::Esp32s3Settings, static_cast<std::size_t>(gpio::Id::COUNT)> gpioConfigs_; 
+    std::array<timer::Esp32s3Settings, static_cast<std::size_t>(timer::Id::COUNT)> timerConfigs_; 
+    serial::Esp32s3Settings serialConfig_;
+    tempsensor::Esp32s3Settings tempsensorConfig_; 
 };
 } // namespace driver::config
---
+++ main/source/driver/config/esp32s3.cpp
@@ -0,0 +1,58 @@
+#include <cstddef>
+#include <thread>
+#include <chrono>
+
+#include "driver/config/esp32s3.h"
+#include "driver/serial/esp32s3.h"
+
+namespace driver::config
+{
+
+Esp32s3::Esp32s3() noexcept
+{
+    auto& led = gpioConfigs_[static_cast<std::size_t>(gpio::Id::LedYellow)];
+    led.isEnabled = true;
+    led.pin = 8U;
+    led.mode = gpio::Mode::Output;
+
+    auto& blinkTimer = timerConfigs_[static_cast<std::size_t>(timer::Id::Blink)];
+    blinkTimer.isEnabled = true;
+    blinkTimer.timeout_ms = 1000U;
+
+    auto& temperatureTimer = timerConfigs_[static_cast<std::size_t>(timer::Id::Temperature)];
+    temperatureTimer.isEnabled = true;
+    temperatureTimer.timeout_ms = 500U;
+
+}
+
+void Esp32s3::delay_ms(std::uint16_t ms) noexcept 
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+const adc::Settings& Esp32s3::getADC(adc::Id id) const noexcept 
+{
+    return adcConfigs_[static_cast<std::size_t>(id)];
+}
+
+const gpio::Settings& Esp32s3::getGpio(gpio::Id id) const noexcept 
+{
+    return gpioConfigs_[static_cast<std::size_t>(id)];
+}
+
+const timer::Settings& Esp32s3::getTimer(timer::Id id) const noexcept 
+{
+    return timerConfigs_[static_cast<std::size_t>(id)];
+}
+
+const serial::Settings& Esp32s3::getSerial() const noexcept 
+{
+    return serialConfig_;
+}
+
+const tempsensor::Settings& Esp32s3::getTempsensor() const noexcept 
+{
+    return tempsensorConfig_;
+}
+
+} // namespace driver::config
